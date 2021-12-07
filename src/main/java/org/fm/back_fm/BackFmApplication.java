0af0e78f0820e7ae0e6480cfb599f7ca8e6bb908
amineferjani <amine.ferjani@gmail.com>
2021-12-07 20:11:01 +0100

    first state app

---
+++ src/main/java/org/fm/back_fm/BackFmApplication.java
@@ -0,0 +1,13 @@
+package org.fm.back_fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackFmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fm/back_fm/BackFmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fm.back_fm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
